af44ec19f05f7bf0eb775bddfc41ab546dec9fff
YevNev <nevzorovyevgen@gmail.com>
2022-08-03 23:28:54 +0300

    Refactor Botik

--- src/Botik.java
+++ src/Botik.java
@@ -4,28 +4,32 @@ import java.util.Arrays;
 
 public class Botik {
 
-
-    public static void main(String[] args) throws URISyntaxException, IOException, InterruptedException {
-
-        System.out.println(TelegramClient.getMessagesUpdate());
-        for (;1 < 2;) {
-            replyMessages();
-        }
-
-
-    }
-    public static void replyMessages() throws URISyntaxException, IOException, InterruptedException {
-
-        String[] messages = TelegramClient.getMessagesUpdate();
 //        4. Отвечать на сообщения в реальном времени
 //        5. Антимат
 //        6. Мини-игра на угадывание слова
-        Arrays.stream(messages)
-                .map(TelegramParser::parseChatId)
-                // если я напишу 2 сообщения, то мне придет 2 ответа (Stream)
-                .distinct()
-                .forEach(TelegramClient::sendMessage);
 
+  public static void main(String[] args)
+      throws URISyntaxException, IOException, InterruptedException {
+
+    System.out.println(TelegramClient.getMessagesUpdate());
+    for (; 1 < 2; ) {
+
+      String[] messages = TelegramClient.getMessagesUpdate();
+      replyMessages(messages);
     }
+
+
+  }
+
+  public static void replyMessages(String[] messages)
+      throws URISyntaxException, IOException, InterruptedException {
+
+    Arrays.stream(messages)
+        .map(TelegramParser::parseChatId)
+        // если я напишу 2 сообщения, то мне придет 2 ответа (Stream)
+        .distinct()
+        .forEach(TelegramClient::sendMessage);
+
+  }
 }
 
